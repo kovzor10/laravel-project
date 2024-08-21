cd58e41814afd726492ad71a5b3843d90c951c23
kovzor <kovzor15@gmail.com>
2024-08-21 17:46:31 +0300

    edit page repair

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -21,6 +21,11 @@
         </div>
 
         <div class="mb-3">
+            <label for="name" class="form-label">Surname</label>
+            <input type="text" class="form-control" id="surname" name="surname" value="{{ $user->surname }}" required>
+        </div>
+
+        <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
         </div>
